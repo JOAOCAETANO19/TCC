dc7409c408a2da2f9eed3cf70d4444e0d1ba524c
JOAOCAETANO19 <joao.cleberson.caetano@escola.pr.gov.br>
2026-04-14 14:52:59 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function mostrarLogin(){
+document.getElementById("login").style.display="flex"
+}
 
+function mostrarCadastro(){
+document.getElementById("cadastro").style.display="flex"
+}
+
+function login(){
+alert("Login realizado!")
+document.getElementById("login").style.display="none"
+}
+
+function cadastrar(){
+alert("Conta criada com sucesso!")
+document.getElementById("cadastro").style.display="none"
+}
+
+let projetos = 0
+
+const botoes = document.querySelectorAll(".card button")
+
+botoes.forEach(botao=>{
+botao.addEventListener("click",()=>{
+projetos++
+document.getElementById("projetosConcluidos").innerText = projetos
+})
+})
